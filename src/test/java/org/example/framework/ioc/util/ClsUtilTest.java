b0ea9c0fd5ced529aa5c5a8e275f83829d9c35c5
vdvhk12 <vdvhk12@gmail.com>
2025-01-06 15:25:13 +0900

    ClsUtil.loadClass, r

---
+++ src/test/java/org/example/framework/ioc/util/ClsUtilTest.java
@@ -0,0 +1,20 @@
+package org.example.framework.ioc.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.example.framework.ioc.util.sample.TestCar;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class ClsUtilTest {
+
+    @Test
+    @DisplayName("ClsUtil.loadClass")
+    public void t1() {
+        Class<TestCar> cls = ClsUtil.loadClass("org.example.framework.ioc.util.sample");
+
+        assertThat(cls).isEqualTo(TestCar.class);
+    }
+
+}
---
+++ src/test/java/org/example/framework/ioc/util/sample/TestCar.java
@@ -0,0 +1,11 @@
+package org.example.framework.ioc.util.sample;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TestCar {
+
+    private String name;
+    private int number;
+
+}
