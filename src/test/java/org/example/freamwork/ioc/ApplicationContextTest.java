b9fefdf0a343d29be2eff841fa929885e76f8cbf
vdvhk12 <vdvhk12@gmail.com>
2025-01-06 14:29:44 +0900

    testPostService, r

--- src/test/java/org/example/freamwork/ioc/ApplicationContextTest.java
+++ src/test/java/org/example/freamwork/ioc/ApplicationContextTest.java
@@ -1,5 +1,7 @@
 package org.example.freamwork.ioc;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +20,13 @@ class ApplicationContextTest {
     @DisplayName("ApplicationContext 객체 생성")
     public void t1() {
         System.out.println(applicationContext);
+    }
+
+    @Test
+    @DisplayName("testPostService 빈 얻기")
+    public void t2() {
+        TestPostService testPostService = applicationContext.genBean("testPostService");
+
+        assertThat(testPostService).isNotNull();
     }
 }
