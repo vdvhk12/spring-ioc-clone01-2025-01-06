dc96cfc7aa0f75f32f3950f18c656089a3531e2c
vdvhk12 <vdvhk12@gmail.com>
2025-01-06 16:26:21 +0900

    ClsUtil.construct, gb

--- src/main/java/org/example/framework/ioc/util/ClsUtil.java
+++ src/main/java/org/example/framework/ioc/util/ClsUtil.java
@@ -1,4 +1,6 @@
 package org.example.framework.ioc.util;
+
+import java.lang.reflect.Constructor;
 import lombok.SneakyThrows;
 
 public class ClsUtil {
@@ -6,5 +8,20 @@ public class ClsUtil {
     @SneakyThrows
     public static <T> Class<T> loadClass(String clsPath) {
         return (Class<T>) Class.forName(clsPath);
+    }
+
+    public static <T> T construct(String clsPath, Object[] args) {
+        return construct(loadClass(clsPath), args);
+    }
+
+    @SneakyThrows
+    public static <T> T construct(Class<T> cls, Object[] args) {
+        Constructor<T> constructor = getConstructor(cls, args);
+
+        return constructor.newInstance(args);
+    }
+
+    public static <T> Constructor<T> getConstructor(Class<T> cls, Object[] args) {
+        return (Constructor<T>) cls.getDeclaredConstructors()[0];
     }
 }
--- src/test/java/org/example/framework/ioc/util/ClsUtilTest.java
+++ src/test/java/org/example/framework/ioc/util/ClsUtilTest.java
@@ -18,9 +18,20 @@ class ClsUtilTest {
     }
 
     @Test
-    @DisplayName("ClsUtil.construct")
+    @DisplayName("ClsUtil.construct with classPath")
     public void t2() {
+        // new TestCar("BMW", 1234);
+        // 아래 코드와 같은 의미
         TestCar testCar = ClsUtil.construct("org.example.framework.ioc.util.sample.TestCar", new Object[]{"BMW", 1234});
+
+        assertThat(testCar.getName()).isEqualTo("BMW");
+        assertThat(testCar.getNumber()).isEqualTo(1234);
+    }
+
+    @Test
+    @DisplayName("ClsUtil.construct with class")
+    public void t3() {
+        TestCar testCar = ClsUtil.construct(TestCar.class, new Object[]{"BMW", 1234});
 
         assertThat(testCar.getName()).isEqualTo("BMW");
         assertThat(testCar.getNumber()).isEqualTo(1234);
