d5ef0f2d34cf2e98023dc6896b0686095bbfd7e9
vdvhk12 <vdvhk12@gmail.com>
2025-01-06 14:50:00 +0900

    testPostService singleton, g

--- src/main/java/org/example/freamwork/ioc/ApplicationContext.java
+++ src/main/java/org/example/freamwork/ioc/ApplicationContext.java
@@ -1,21 +1,32 @@
 package org.example.freamwork.ioc;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.example.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
+    private Map<String, Object> beans;
     private String basePackage;
 
     public ApplicationContext(String basePackage) {
         this.basePackage = basePackage;
+        this.beans = new HashMap<>();
     }
 
     public void init() {
     }
 
     public <T> T genBean(String beanName) {
-        if (beanName.equals("testPostService")) {
-            return (T) new TestPostService();
+        Object bean = beans.get(beanName);
+
+        if (bean == null) {
+            bean = switch (beanName) {
+                case "testPostService" -> new TestPostService();
+                default -> null;
+            };
+            beans.put(beanName, bean);
         }
-        return null;
+
+        return (T) bean;
     }
 }
